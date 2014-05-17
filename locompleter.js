92d27ce5c1ed913d486380c811ebb8fc5bd12d7e
mflerin <marc@flerin.com>
2014-05-17 11:39:32 +0100

    Update the directive
    So it suits my own needs a bit better.

--- locompleter.js
+++ locompleter.js
@@ -1,61 +1,105 @@
-/* global google */
 
 angular.module('locompleter', [])
-  .factory('googleMaps', ['$window',
-    function ($window) {
-      return {
-        init: function (callback) {
-          // Don't init twice
-          if (!$window.google) {
-            $window.onInit = callback; // Google maps calls `onInit` when loaded
+    .factory('googleMaps', [
+        '$window',
+        function ($window) {
+            return {
+                init: function (callback) {
+                    // Don't init twice
+                    if (!$window.google) {
+                        $window.onInit = callback; // Google maps calls `onInit` when loaded
 
-            var script = document.createElement('script');
+                        var script = document.createElement('script');
 
-            script.type = 'text/javascript';
-            script.src = 'https://maps.googleapis.com/maps/api/js?libraries=places&sensor=false&callback=onInit';
+                        script.type = 'text/javascript';
+                        script.src = 'https://maps.googleapis.com/maps/api/js?libraries=places&sensor=false&callback=onInit';
 
-            document.body.appendChild(script);
-          } else {
-            callback();
-          }
-        }
-      };
-    }
-  ])
-  .directive('autocompleteLocation', function () {
-    return {
-      restrict: 'A',
-      controller: ['$element', '$scope', 'googleMaps',
-        function ($element, $scope, googleMaps) {
-          googleMaps.init(function onReady() {
-            var autocomplete = new google.maps.places.Autocomplete($element[0], {});
-
-            autocomplete.addListener('place_changed', function () {
-              var placeData = autocomplete.getPlace();
-
-              var city;
-              var country;
-
-              placeData.address_components.forEach(function (component) {
-                if (component.types[0] === 'locality') {
-                  city = component.long_name;
-                } else if (component.types[0] === 'country') {
-                  country = component.long_name;
+                        document.body.appendChild(script);
+                    } else {
+                        callback();
+                    }
                 }
-              });
-
-              $scope.$emit('locationAutocompleted', {
-                latitude: placeData.geometry.location.lat(),
-                longitude: placeData.geometry.location.lng(),
-                city: city,
-                country: country
-              });
-
-              // Force view model to update after autocompletion
-              $element.triggerHandler('input');
-            });
-          });
+            };
         }
-      ]
-    };
-  });
+    ])
+    .directive('autocompleteLocation', [
+        '$window', 'googleMaps', function ($window, googleMaps) {
+            //Usage:
+            // <input autocomplete-location type = "text">
+            var directive = {
+                link: link,
+                restrict: 'A',
+            };
+            return directive;
+
+            function link(scope, element) {
+
+                var autocomplete;
+
+                googleMaps.init(initialize);
+
+                function initialize() {
+                    autocomplete = new $window.google.maps.places.Autocomplete(element[0], {});
+                    autocomplete.addListener('place_changed', onPlaceChanged);
+                };
+
+                function onPlaceChanged() {
+                    var placeData = autocomplete.getPlace();
+
+                    var address = getAddress(placeData.address_components);
+
+                    var addressArray = [address.streetNumber,
+                                        address.street,
+                                        address.town,
+                                        address.city,
+                                        address.county,
+                                        address.postcode];
+
+                    addressArray = addressArray.filter(function (item) {
+                        return angular.isDefined(item);
+                    });
+
+                    scope.$emit('locationAutocompleted', {
+                        latitude: placeData.geometry.location.lat(),
+                        longitude: placeData.geometry.location.lng(),
+                        address: address,
+                        fullAddress: addressArray.join(', ')
+                    });
+                };
+
+                function getAddress(components) {
+
+                    var address = {};
+
+                    //iterate over components and pull out the address details;
+                    components.forEach(function (component) {
+
+                        switch (component.types[0]) {
+                            case "street_number":
+                                address.streetNumber = component.long_name;
+                                break;
+                            case "route":
+                                address.street = component.long_name;
+                                break;
+                            case "neighborhood":
+                                address.town = component.long_name;
+                                break;
+                            case "locality":
+                                address.city = component.long_name;
+                                break;
+                            case "administrative_area_level_2":
+                                address.county = component.long_name;
+                                break;
+                            case "postal_code":
+                                address.postcode = component.long_name;
+                                break;
+                            default:
+                                break;
+                        }
+
+                    });
+                    return address;
+                }
+
+            }
+        }]);
